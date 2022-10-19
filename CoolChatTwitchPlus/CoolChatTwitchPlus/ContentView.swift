c930fb376fd9bab2023861a895aa4b22d55fbaa2
mwilson <maegan@maeganwilson.com>
2022-10-18 20:22:27 -0500

    redemption: mikaelacaron

---
+++ CoolChatTwitchPlus/CoolChatTwitchPlus/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  CoolChatTwitchPlus
+//
+//  Created by Maegan Wilson on 10/18/22.
+//
+//  Comment Redemptions:
+//  MikaelaCaron 2022-10-18
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoolChatTwitchPlus/CoolChatTwitchPlus/CoolChatTwitchPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolChatTwitchPlusApp.swift
+//  CoolChatTwitchPlus
+//
+//  Created by Maegan Wilson on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolChatTwitchPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
