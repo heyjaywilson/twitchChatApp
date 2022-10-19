1410e577826478307958b7321bcfd4f619384a2a
mwilson <maegan@maeganwilson.com>
2022-10-18 21:15:25 -0500

    SUCCESSFULLY READING CHAT

--- CoolChatTwitchPlus/CoolChatTwitchPlus/ContentView.swift
+++ CoolChatTwitchPlus/CoolChatTwitchPlus/ContentView.swift
@@ -9,16 +9,45 @@
 //
 
 import SwiftUI
+import TwitchChat
+
+extension ChatMessage: Identifiable {
+    public var id: UUID {
+        UUID()
+    }
+}
+
+class CCTChat: ObservableObject {
+    @Published var lastChat: [ChatMessage] = []
+
+    let chat = TwitchChat(token: ProcessInfo.processInfo.environment["UserToken"]!, name: "mwilson_codes")
+
+    func startListening() async {
+        do {
+            for try await message in chat.messages {
+                print("message received")
+                lastChat.append(message)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
 
 struct ContentView: View {
+    @StateObject var chat = CCTChat()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Chat")
+            List(chat.lastChat) { chatMessage in
+                Text(chatMessage.text)
+            }
+        }.task {
+            await chat.startListening()
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
